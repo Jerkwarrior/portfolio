853930df365d20f3517b273b72352b4700108a80
Luke Marco <luke.am89@hotmail.co.uk>
2017-11-28 14:48:10 +0000

    Production Test

--- dist/server.js
+++ dist/server.js
@@ -9,9 +9,7 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
 
-app.use(staticFileMiddleware);
 app.use(history());
-app.use(staticFileMiddleware);
 
 var port = process.env.PORT || 5000;
 app.listen(port);
